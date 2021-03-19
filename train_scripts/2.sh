fc1a71759a2bf0fa022f3afff442264444ef16a6
ileana <irugina@mit.edu>
2021-03-19 00:13:52 -0400

    add train scripts

--- train_scripts/2.sh
+++ train_scripts/2.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #SBATCH --cpus-per-task=40
 #SBATCH --gres=gpu:volta:2
-#SBATCH -o logs/1.out
-#SBATCH --job-name=1
+#SBATCH -o logs/2.out
+#SBATCH --job-name=2
 
 python -u main.py \
 	--batch_size=4 \
 	--n_support=10 \
 	--n_query=10 \
 	--loss_function=reconstruction \
-	--optimization=joint \
+	--optimization=maml \
         --resize_target \
         --target_size=384 \
 	--device=cuda \
