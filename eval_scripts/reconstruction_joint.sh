bd9609fe259c2a5a4783cc867eee92b15c91eef6
ileana <irugina@mit.edu>
2021-03-29 20:46:25 -0400

    add sample eval script

---
+++ eval_scripts/reconstruction_joint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH --cpus-per-task=20
+#SBATCH --gres=gpu:volta:1
+#SBATCH -o logs/reconstruction_joint_eval.out
+#SBATCH --job-name=reconstruction_joint_eval
+
+python -u eval_unet.py \
+	--checkpoint=reconstruction_joint/
