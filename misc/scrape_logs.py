0b233b31292e7ec422cd1e1110348f58c5227c0f
ileana <irugina@mit.edu>
2021-05-02 23:47:25 -0400

    make log scrape work with GAN format

--- misc/scrape_logs.py
+++ misc/scrape_logs.py
@@ -5,14 +5,17 @@ def parse(path):
     lines = []
     while True:
         line = file.readline()
-        if line[0:14] == "loss at epoch ":
+        if line[0:14] == "loss at epoch " and "step" in line:
             line = line.split()
             lines.append(line)
 
             epoch = int(line[3][0:-1])
             step = int(line[5])
             total_steps = int(line[8][0:-1])
-            loss = float(line[-1])
+            try:
+                loss = float(line[-1])
+            except:
+                loss = float(line[-1][0:-1])
 
             steps.append(float(epoch * total_steps + step) / total_steps)
             losses.append(loss)
