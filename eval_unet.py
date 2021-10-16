3a96072924fc40923846c9f25b305cfc9d42626f
ileana <irugina@mit.edu>
2021-10-16 16:44:19 -0400

    refactor eval

--- eval_unet.py
+++ eval_unet.py
@@ -17,43 +17,42 @@ from utils.eval import *
 from metrics.metric import *
 from misc.scrape_logs import *
 
-def intermediate_output(results, count, src_img, prediction, tgt_img):
-    # evaluate weather metrics
-    for key in results:
-        print ("for key {} estimate after {} test batches is {}".format(key, count, results[key] / count))
-    # save samples to disk
-    np.save(os.path.join(opt.checkpoint, 'source_{}'.format(count)), src_img.detach().cpu().numpy())
-    np.save(os.path.join(opt.checkpoint, 'prediction_{}'.format(count)), prediction.detach().cpu().numpy())
-    np.save(os.path.join(opt.checkpoint, 'target_{}'.format(count)), tgt_img.detach().cpu().numpy())
 
-def find_best_checkpoint(log_path):
-    steps, losses, lines = parse(log_path)
-    best_step = np.argmin(losses)
-    line = lines[best_step]
-    print ("loading checkpoint: ", line)
-    epoch = int(line[3][0:-1])
-    step = int(line[5])
-    return epoch, step
-
-def load_reconstruction_generator(opt, epoch, step):
+def load_reconstruction_generator(opt, checkpoint):
     model = Unet()
-    model.load_state_dict(torch.load(os.path.join(opt.checkpoint, "checkpoint_epoch_{}_step_{}.pt".format(epoch, step))))
+    if opt.multi_gpu:
+        model = model.to(opt.device)
+        model = nn.DataParallel(model, [0,1])
+    model.load_state_dict(torch.load(checkpoint))
     model.eval()
     return model
 
-def load_adversarial_model(opt, epoch, step):
+def load_adversarial_model(opt, unet_checkpoint, discriminator_checkpoint):
     # generator
     generator = Unet()
-    generator.load_state_dict(torch.load(os.path.join(opt.checkpoint, "generator_epoch_{}_step_{}.pt".format(epoch, step))))
+    if opt.multi_gpu:
+        generator = generator.to(opt.device)
+        generator = nn.DataParallel(generator, [0,1])
+    generator.load_state_dict(torch.load(unet_checkpoint))
     generator.eval()
     # discriminator
     discriminator = NLayerDiscriminator(opt.input_nc + opt.output_nc)
-    discriminator.load_state_dict(torch.load(os.path.join(opt.checkpoint, "discriminator_epoch_{}_step_{}.pt".format(epoch, step))))
+    if opt.multi_gpu:
+        discriminator = discriminator.to(opt.device)
+        discriminator = nn.DataParallel(discriminator, [0,1])
+    discriminator.load_state_dict(torch.load(discriminator_checkpoint))
     discriminator.eval()
     # return
     return generator, discriminator
 
-def evaluate_prediction(vil_znorm, src_img, tgt_img, prediction, results, count):
+def load_model(opt, unet_checkpoint, discriminator_checkpoint):
+    if opt.loss_function == "adversarial":
+        assert opt.discriminator_checkpoint is not None
+        return load_adversarial_model(opt, unet_checkpoint, discriminator_checkpoint)
+    return load_reconstruction_generator(opt, unet_checkpoint)
+
+
+def evaluate_prediction(vil_znorm, tgt_img, prediction, results):
     # undo norm to use thresholds
     vil_mu, vil_sigma = vil_znorm
     tgt_img = vil_mu + tgt_img * vil_sigma
@@ -62,38 +61,37 @@ def evaluate_prediction(vil_znorm, src_img, tgt_img, prediction, results, count)
     metrics = compute_metrics(tgt_img, prediction, metric_functions)
     for key in results:
         results[key] += metrics[key]
-    if (count + 1) % 20 == 0:
-        intermediate_output(results, count, src_img, prediction, tgt_img)
+
 
 if __name__ == "__main__":
-    # process single cli arg and load opt from train
+    # process cli arg
     parser = argparse.ArgumentParser()
-    parser.add_argument('--checkpoint', type=str, required=True)
-    parser.add_argument('--log', type=str, required=True)
+    parser.add_argument('--unet_checkpoint', type=str, required=True)
+    parser.add_argument('--discriminator_checkpoint', type=str, default=None)
+    parser.add_argument('--path_to_opt', type=str, required=True)
+    parser.add_argument('--phase', type=str, required=True)
     opt = parser.parse_args()
-    log_path = opt.log
-    checkpoint = opt.checkpoint
-
-    with open(os.path.join(opt.checkpoint, "opt.txt"), 'r') as f:
+    # save cli args
+    unet_checkpoint = opt.unet_checkpoint
+    discriminator_checkpoint = opt.discriminator_checkpoint
+    path_to_opt = opt.path_to_opt
+    phase = opt.phase
+    # load experiment opt from disk
+    with open(path_to_opt,  'r') as f:
         opt.__dict__ = json.load(f)
-    opt.checkpoint = checkpoint
 
-    # find checkpoint to load
-    epoch, step = find_best_checkpoint(log_path)
-
-    if "checkpoint" not in opt.checkpoint:
-        opt.checkpoint = os.path.join("checkpoints", opt.checkpoint)
+    # dataset and loader
+    opt.phase = phase
+    dataset = SevirDataset(opt)
+    dataloader = DataLoader(dataset, batch_size=opt.batch_size)
 
     # model
-    if opt.loss_function == "reconstruction":
-        model = load_reconstruction_generator(opt, epoch, step)
-    if opt.loss_function == "adversarial":
-        generator, discriminator = load_adversarial_model(opt, epoch, step)
+    model = load_model(opt, unet_checkpoint, discriminator_checkpoint)
 
-    # data
-    opt.phase = "test"
-    test_dataset = SevirDataset(opt)
-    test_dataloader = DataLoader(test_dataset, batch_size=1)
+    # run eval
+    eval_fn = eval("eval_{}_{}".format(opt.optimization, opt.loss_function))
+    result = eval_fn (model, dataloader, opt)
+    print (result)
 
     # get metric functions and names
     metric_functions = get_metric_functions()
@@ -105,47 +103,15 @@ if __name__ == "__main__":
         results[key] = 0
 
     # zscore normalization
-    vil_znorm = test_dataset.znorm['vil']
+    vil_znorm = dataset.znorm['vil']
 
-    # compute metrics
-    for count, test_batch in enumerate(test_dataloader):
-        if opt.optimization == "joint": #this is easy: no adaptatation
-            # put unet on GPU - regardless of how it was trained
+    for count, batch in enumerate(dataloader):
+        if opt.optimization == "joint":
             if opt.loss_function == "reconstruction":
                 model = model.to(opt.device)
             if opt.loss_function == "adversarial":
                 model = generator.to(opt.device)
-            # flatten first two axis - don't care about per event classification of different frames
-            src_img = test_batch['A'].view(-1, *(test_batch['A'].size()[2:])).float()
-            tgt_img = test_batch['B'].view(-1, *(test_batch['B'].size()[2:])).float()
-            # fwd pass
-            src_img, tgt_img = src_img.to(opt.device), tgt_img.to(opt.device)
+            src_img, tgt_img = flatten_for_joint(batch, opt.device)
             prediction = model(src_img)
-
-        # NOTE code below makes sense iff test dataloader has batch_size == 1
-        if opt.optimization == "maml" and opt.loss_function == "adversarial":
-            generator, discriminator = generator.to(opt.device), discriminator.to(opt.device)
-            model = (generator, discriminator)
-            for task_idx in range(test_batch['A'].size()[0]): #for each test task
-                task_data = {
-                        'A': test_batch['A'][task_idx, :, :, :, :],
-                        'B': test_batch['B'][task_idx, :, :, :, :],
-                    }
-                src_img, prediction, tgt_img = adapt_adversarial(model, opt, task_data, True)
-
-        if opt.optimization == "maml" and opt.loss_function == "reconstruction":
-            model = model.to(opt.device)
-            for task_idx in range(test_batch['A'].size()[0]): #for each test task
-                task_data = {
-                        'A': test_batch['A'][task_idx, :, :, :, :].float(),
-                        'B': test_batch['B'][task_idx, :, :, :, :].float(),
-                    }
-                src_img, prediction, tgt_img = adapt_reconstruction(model, task_data, opt, True)
-
-        # eval weather metrics and save some samples to disk
-        evaluate_prediction(vil_znorm, src_img, tgt_img, prediction, results, count)
-
-    # norm by number of batches
-    for key in results:
-        results[key] /= len(test_dataloader)
-    print (results)
+            evaluate_prediction(vil_znorm, tgt_img, prediction, results)
+            print (result)
--- utils/eval.py
+++ utils/eval.py
@@ -1,8 +1,8 @@
 import torch
-
 # local
 from utils.l2l import *
 from utils.gan_loss import *
+from utils.common_helpers import *
 
 def eval_joint_adversarial(model, dataloader, opt):
     # loss objectives
@@ -10,76 +10,74 @@ def eval_joint_adversarial(model, dataloader, opt):
     criterionL1 = torch.nn.L1Loss()
     # unpack model
     generator, discriminator = model
-    total_loss_D, total_loss_G, total_loss_G_L1 = 0, 0, 0
+    total_loss_D_fake, total_loss_D_real, total_loss_G_GAN, total_loss_G_L1 = 0, 0, 0, 0
     for eval_batch in dataloader:
-        # flatten first two axis - don't care about per event classification of different frames
-        src_img = eval_batch['A'].view(-1, *(eval_batch['A'].size()[2:])).float()
-        tgt_img = eval_batch['B'].view(-1, *(eval_batch['B'].size()[2:])).float()
-        src_img, tgt_img = src_img.to(opt.device), tgt_img.to(opt.device)
-        prediction = generator(src_img)
+        src_img, tgt_img = flatten_for_joint (eval_batch, opt.device)
+        pred = generator(src_img)
         resized_src_img = nn.Upsample(scale_factor=2, mode='bilinear') (src_img)
-        # -------------------------------------------------------------------------------- discriminator loss
-        # fake
-        fake_AB = torch.cat((resized_src_img, prediction), 1)
-        pred_fake = discriminator(fake_AB.detach()) # stop backprop to the generator by detaching fake_B
-        loss_D_fake = criterionGAN(pred_fake, False)
-        # real
-        real_AB = torch.cat((resized_src_img, tgt_img), 1)
-        pred_real = discriminator(real_AB)
-        loss_D_real = criterionGAN(pred_real, True)
-        # combine loss
-        loss_D = (loss_D_fake + loss_D_real) * 0.5
-        # -------------------------------------------------------------------------------- generator loss
-        # gan loss
-        loss_G_GAN = criterionGAN(pred_fake, True)
-        # reconstruction loss
-        loss_G_L1 = criterionL1(prediction, tgt_img) * opt.lambda_L1
-        # combine loss
-        loss_G = loss_G_GAN + loss_G_L1
-        # -------------------------------------------------------------------------------- keep track of loss over whole dataset
-        total_loss_D += loss_D.item()
-        total_loss_G += loss_G.item()
-        total_loss_G_L1 += loss_G_L1.item()
-    return total_loss_D / len(dataloader), total_loss_G / len(dataloader), total_loss_G_L1 / len(dataloader)
+        # compute losses
+        loss_D_fake, loss_D_real, loss_G_GAN, loss_G_L1 = compute_GAN_loss(resized_src_img, tgt_img, pred, discriminator, criterionGAN, criterionL1, opt.lambda_L1)
+        total_loss_D_fake += loss_D_fake.item(); total_loss_D_real += loss_D_real.item()
+        total_loss_G_GAN += loss_G_GAN.item(); total_loss_G_L1 += loss_G_L1.item()
+    result = dict()
+    result['loss_D_fake'] = total_loss_D_fake; result['loss_D_real'] = total_loss_D_real
+    result['loss_G_GAN'] = total_loss_G_GAN; result['loss_G_L1'] = total_loss_G_L1
+    for key, value in result.items():
+            value /= len(dataloader)
+    return result
 
 def eval_maml_adversarial(model, dataloader, opt):
     generator, discriminator = model
     loss_D, loss_G, loss_G_L1 = 0, 0, 0
     for eval_batch in dataloader:
         for task_idx in range(eval_batch['A'].size()[0]): #for each task
-            task_data = {
-                    'A': eval_batch['A'][task_idx, :, :, :, :],
-                    'B': eval_batch['B'][task_idx, :, :, :, :],
-                }
-            task_loss_D, task_loss_G, task_loss_G_L1, _, _ = adapt_adversarial(model, opt, task_data)
-            loss_D += task_loss_D.item() / opt.batch_size
-            loss_G += task_loss_G.item() / opt.batch_size
-            loss_G_L1 += task_loss_G_L1.item() / opt.batch_size
-    return loss_D / len(dataloader), loss_G / len(dataloader), loss_G_L1 / len(dataloader)
+            # adapt to task using support set
+            task_data = pack_for_maml(eval_batch, task_idx)
+            _, _, task_discriminator, task_generator = adapt_adversarial(model, opt, task_data)
+            # fwd pass on query set
+            _, _, query_real_A , query_real_B = unpack_data(data, opt)
+            pred = task_generator(query_real_A)
+            resized_src_img = nn.Upsample(scale_factor=2, mode='bilinear') (query_real_A)
+            # compute losses on query 
+            loss_D_fake, loss_D_real, loss_G_GAN, loss_G_L1 = compute_GAN_loss(
+                    resized_src_img, 
+                    query_real_B, 
+                    pred, 
+                    task_discriminator, 
+                    criterionGAN, 
+                    criterionL1, 
+                    opt.lambda_L1)
+            total_loss_D_fake += loss_D_fake.item(); total_loss_D_real += loss_D_real.item()
+            total_loss_G_GAN += loss_G_GAN.item(); total_loss_G_L1 += loss_G_L1.item()
+    result = dict()
+    result['loss_D_fake'] = total_loss_D_fake; result['loss_D_real'] = total_loss_D_real
+    result['loss_G_GAN'] = total_loss_G_GAN; result['loss_G_L1'] = total_loss_G_L1
+    for key, value in result.items():
+            value /= len(dataloader) 
+            value /= opt.batch_size
+    return result
 
 def eval_joint_reconstruction(model, dataloader, opt):
     total_loss = 0
     loss_fn = torch.nn.L1Loss()
     for eval_batch in dataloader:
-        # flatten first two axis - don't care about per event classification of different frames
-        src_img = eval_batch['A'].view(-1, *(eval_batch['A'].size()[2:])).float()
-        tgt_img = eval_batch['B'].view(-1, *(eval_batch['B'].size()[2:])).float()
-        src_img, tgt_img = src_img.to(opt.device), tgt_img.to(opt.device)
-        # compute loss
+        src_img, tgt_img = flatten_for_joint (eval_batch, opt.device)
         prediction = model(src_img)
+        # compute loss
         loss = loss_fn(tgt_img, prediction)
-        total_loss += loss.item()
-    return total_loss / len(dataloader)
+        total_loss += loss_fn(tgt_img, prediction).item()
+    result = dict()
+    result['MAE'] = total_loss / len(dataloader)
+    return result 
 
 def eval_maml_reconstruction(model, dataloader, opt):
     total_loss = 0
     for eval_batch in dataloader:
         # data is meta-batch of eval tasks - adapt and track loss on each
         for task_idx in range(eval_batch['A'].size()[0]): #for each task
-            task_data = {
-                    'A': eval_batch['A'][task_idx, :, :, :, :],
-                    'B': eval_batch['B'][task_idx, :, :, :, :],
-                    }
-            task_loss = adapt_reconstruction(model, task_data, opt)
+            task_data = pack_for_maml(eval_batch, task_idx)
+            task_loss, task_model= adapt_reconstruction(model, task_data, opt)
             total_loss += task_loss.item() / opt.batch_size
-    return total_loss / len(dataloader)
+    result = dict()
+    result['MAE'] = total_loss / len(dataloader)
+    return result 
